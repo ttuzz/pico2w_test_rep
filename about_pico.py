f03c2925d357a31578cec7c3c5899fe10caedf88
Özcan Özkan <11372179+ttuzz@users.noreply.github.com>
2025-06-21 16:51:52 +0300

    +

---
+++ about_pico.py
@@ -0,0 +1,55 @@
+import ntptime
+ntptime.settime()
+
+import time
+print("Zaman:", time.localtime())
+
+with open("veri.txt", "w") as f:
+    f.write("Merhaba Raspberry Pi Pico!\n")
+    f.write("Bu bir dosya yazma örneğidir.")
+
+with open("veri.txt", "r") as f:
+    icerik = f.read()
+    print("Dosya İçeriği:\n", icerik)
+    
+    
+    
+import gc
+
+gc.collect()
+free = gc.mem_free()
+alloc = gc.mem_alloc()
+total = free + alloc
+
+print("Toplam RAM:", total, "bayt")
+print("Kullanılan:", alloc, "bayt")
+print("Boş:", free, "bayt")
+
+import os
+
+fs_stat = os.statvfs("/")
+
+block_size = fs_stat[0]
+total_blocks = fs_stat[2]
+free_blocks = fs_stat[3]
+
+total_bytes = block_size * total_blocks
+free_bytes = block_size * free_blocks
+used_bytes = total_bytes - free_bytes
+
+print("Toplam Flash:", total_bytes, "bayt")
+print("Kullanılan:", used_bytes, "bayt")
+print("Boş:", free_bytes, "bayt")
+
+
+import time
+
+start = time.ticks_ms()
+
+# yoğun işlem
+for _ in range(1000000):
+    pass
+
+elapsed = time.ticks_diff(time.ticks_ms(), start)
+print("Geçen süre (ms):", elapsed)
+
